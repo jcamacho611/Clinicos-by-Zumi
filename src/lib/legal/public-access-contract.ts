9dfdecfbe20665379a9e60f67127ec775f911a35
jcamacho611 <justinrcamacho@gmail.com>
2026-08-27 16:32:15 -0400

    feat: share public access agreement contract

---
+++ src/lib/legal/public-access-contract.ts
@@ -0,0 +1,4 @@
+export const PUBLIC_ACCESS_TERMS_KEY = "access-confidentiality-ip";
+export const PUBLIC_ACCESS_TERMS_VERSION = "2026-08-10.1";
+export const PUBLIC_ACCESS_STORAGE_KEY = "klinikos-public-access-version";
+export const PUBLIC_ACCESS_EMAIL_STORAGE_KEY = "klinikos-public-access-email";
